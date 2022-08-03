245611a060611dc85a948bdba90777d802d3583e
ksm1996 <sarpongmensahk@gmail.com>
2022-08-03 18:47:42 +0000

    Final changes

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,19 @@
+def english_To_French(englishText):
+
+    #This function tests for null input for french to english
+
+    self.assertEqual(translate(null), null)
+
+    self.assertEqual(translate('Hello'), 'Bonjour')
+
+    return frenchText
+
+def french_To_English(frenchText):
+
+    #This function tests for null input for english to french
+
+    self.assertEqual(translate(null), null)
+
+    self.assertEqual(translate('Bonjour'), 'Hello')
+    
+    return englishText
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,25 @@
+import json
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+
+authenticator = IAMAuthenticator('{apikey}')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('{url}')
+
+translation = language_translator.translate(
+    text='Hello',
+    model_id='en-fr').get_result()
+print(json.dumps(translation, indent=2, ensure_ascii=False))
